888fcbde22bc80593c66d05713c3e7c01f0f1023
Vadim Leontiev <vleontiev@englishdom.com>
2017-10-13 18:01:46 +0300

    LK-10049 Запуск дефолтного контейнера

--- config/config.php
+++ config/config.php
@@ -11,6 +11,7 @@ $cacheConfig = [
 ];
 
 $aggregator = new ConfigAggregator([
+    \Zend\Log\ConfigProvider::class,
     // Include cache configuration
     new ArrayProvider($cacheConfig),
     // Base config provider
---
+++ library/Common/Middleware/PrepareImageMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Common\Middleware;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Class PrepareImageMiddleware
+ * @package Common\Middleware
+ */
+class PrepareImageMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate)
+    {
+        // TODO: Implement process() method.
+    }
+}
