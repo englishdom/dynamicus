f52f2c8feb422592ee2f859ecbd025ad8582d757
Vadim Leontiev <vleontiev@englishdom.com>
2017-10-13 15:21:48 +0300

    LK-10049 Создание нового проекта

---
+++ config/autoload/images.global.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'translation' => [
+        'default' => [
+            [66, 50], // skills/glossary/learning/18014/ translations images in list
+            [240, 168], // skills/glossary/learning/18014/#word/like images for translations
+        ],
+    ],
+    'word-set' => [
+        'default' => [
+            [180, 115], // skills/glossary/ word sets list
+        ]
+    ],
+    'course-int' => [
+        'default' => [
+            [960, 172], // Course lesson header new image
+        ],
+        'exercise' => [
+            [940, 532], // Courses-int, exercises, group B
+            [600, 432], // Courses-int, exercises, group C
+            [940, 293], // Courses-int, LifeStory
+        ]
+    ],
+    'ed-class' => [
+        'default' => [
+            [300, 181], // Ed Class small image
+            [940, 384], // Ed Class big image
+            [940, 172], // Ed lesson
+        ]
+    ]
+];
+
+// [100, 100], //Undefined size
+// [300, 194], //Undefined size
+// [300, 190], //Undefined size
