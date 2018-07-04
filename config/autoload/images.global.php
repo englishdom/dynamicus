3721a01403c1a40b44c31e337ac121c53bd41eb9
Vadim Leontiev <vleontiev@englishdom.com>
2018-07-04 17:51:01 +0300

    Added new namespaces for certificates

--- config/autoload/images.global.php
+++ config/autoload/images.global.php
@@ -136,11 +136,11 @@ return [
         'certificate' => [
             KEY_ORIGINAL => true // save only original
         ],
-        'certificate-share' => [
-            KEY_DEFAULT => [
-                [WIDTH => 1200, HEIGHT => 630, QUALITY => 100], // fb
-                [WIDTH => 537, HEIGHT => 240, QUALITY => 100], // vk
-            ]
+        'certificate-share-fb' => [
+            KEY_ORIGINAL => true // save only original
+        ],
+        'certificate-share-vk' => [
+            KEY_ORIGINAL => true // save only original
         ],
         'online-test' => [
             KEY_DEFAULT => [
