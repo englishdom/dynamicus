715e57f5636b43e72ba288ff719aa556374ba7cd
Vadim Leontiev <vleontiev@englishdom.com>
2018-07-13 17:06:45 +0300

    Added new entity `course-int-lesson` for work with selectel adapter

--- config/autoload/hosts.global.php
+++ config/autoload/hosts.global.php
@@ -4,11 +4,13 @@ return [
     'hosts' => [
         '0' => env('HOST_LOCAL'),
         'faq' => env('HOST_SELECTEL'),
+        'course-int-lesson' => env('HOST_SELECTEL'),
     ],
     'adapters' => [
         '0' => \League\Flysystem\AdapterInterface::class,
         /* Для entity FAQ будет использвоаться адаптер selectel */
         'faq' => \Common\Container\SelectelAdapterInterface::class,
+        'course-int-lesson' => \Common\Container\SelectelAdapterInterface::class,
     ],
 
 ];
